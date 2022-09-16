a3d04362bf58633da493c2f4141c148a171b815d
Henry <61155410+ParkJsoo@users.noreply.github.com>
2022-09-16 12:31:17 +0900

    [section8-15] 포스트 데이터를 가져와서 보여주기(remark)
    - remark 라이브러리를 사용해 상세 데이터 출력

--- lib/post.ts
+++ lib/post.ts
@@ -1,6 +1,8 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs         from 'fs';
+import path       from 'path';
+import matter     from 'gray-matter';
+import {remark}   from 'remark';
+import remarkHtml from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
@@ -13,13 +15,13 @@ export function getSortedPostsData() {
     const id = fileName.replace(/\.md$/, '');
 
     const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
+    const fileContents = fs.readFileSync(fullPath, 'utf-8')
 
     const matterResult = matter(fileContents);
 
     return {
       id,
-      ...matterResult.data as {date: string; title: string}
+      ...matterResult.data as { date: string; title: string }
     }
   })
 
@@ -31,4 +33,32 @@ export function getSortedPostsData() {
       return -1
     }
   })
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, '')
+      }
+    }
+  })
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf-8');
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark().use(remarkHtml).process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as {date: string; title: string})
+  }
 }
